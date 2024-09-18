0370beb042f10011489d787e315a3f7c0aa103dd
dasickboiz <nguyenkhuat29@gmail.com>
2024-09-18 09:28:42 +0700

    day_2 commit

--- Day_1/main.py
+++ Day_1/main.py
@@ -1,2 +1,16 @@
-print("Hello World😂")
-print("2222222222222222222222222")
+print(type(1))
+print(type(1.2))
+print(type(""))
+print(type(True))
+print(type(1 + 3j))
+
+x = 5
+print(type(x))
+
+a_ = 2003
+print(a_)
+
+full_name = input("Nhap ho va ten: ")
+print(full_name)
+age = int(input("Nhap tuoi: "))
+print("Tuoi: ", age)
---
+++ Day_2/main.py
@@ -0,0 +1,33 @@
+print(1+2)
+print(1-2)
+print(1*2)
+print(1**2)
+print(1/2)
+print(1%2)
+print(1//2)
+print("------------------------------------------------------")
+print(5 > 3)
+print("------------------------------------------------------")
+
+print(True and True)
+print(True or False)
+print(not True)
+print(2 and 3)
+print(2 or 1)
+print("------------------------------------------------------")
+a = int(input("Nhap a: "))
+b = int(input("Nhap b: "))
+
+print("{0} + {1} = {2}".format(a, b, a+b))
+print("{0} - {1} = {2}".format(a, b, a-b))
+print("{0} * {1} = {2}".format(a, b, a*b))
+print("{0} ** {1} = {2}".format(a, b, a**b))
+print("{0} / {1} = {2}".format(a, b, a/b))
+print("{0} // {1} = {2}".format(a, b, a//b))
+print("{0} % {1} = {2}".format(a, b, a%b))
+
+print("------------------------------------------------------")
+
+age = 40
+print(f"I am {age}")
+print("I am {}".format(age))
